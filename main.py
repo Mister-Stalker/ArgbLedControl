6fc0a7f1578ab6bc8922d1b1d1ff380c1276346c
Mister-Stalker <pna24052006@yandex.ru>
2021-12-27 12:58:16 +0300

    test1

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
+from abc import ABC
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from kivy.lang import Builder
+
+from kivymd.app import MDApp
+from kivymd.uix.bottomnavigation import MDBottomNavigationItem
+from kivymd.uix.screen import MDScreen
+class ArgbLedControl(MDApp):
+
+    def build(self):
+        self.theme_cls.material_style = "M3"
+        return Builder.load_file("app_interface.kv")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class MDScreenMain(MDScreen):
+    def say_hello(self,arg=''):
+        print(f"hello from {arg}")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+ArgbLedControl().run()
