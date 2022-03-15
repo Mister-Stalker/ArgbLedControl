c747c148a9bc539daecf6fb8c751871ae40c8eee
Mister-Stalker <pna24052006@yandex.ru>
2022-03-15 10:00:38 +0300

    Update esp_connection.py

--- esp_connection.py
+++ esp_connection.py
@@ -4,8 +4,9 @@ import time
 
 
 class EspConnection:
-    def __init__(self, ip="192.168.0.123", port=80):
+    def __init__(self, app_ids, ip="192.168.0.123", port=80):
         self.ip = ip
+        self.app_ids = app_ids
         self.port = port
         self.data = {
             "temp": {},
@@ -14,9 +15,8 @@ class EspConnection:
             "lock": False,  # блокировка при обмене данных для избежания сбоев
             "brightness": 10,
         }
-
+    
     def _send_and_read(self, msg: str, sleep=0.5, read=True):
-
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((self.ip, self.port))
         msg = msg.encode()
@@ -25,8 +25,12 @@ class EspConnection:
             time.sleep(sleep)
             r = sock.recv(4096).decode()
             return r
-
+        
     def command(self, msg, flag="-m"):
+        thread = threading.Thread(target=self._command, args=(command, flag))
+        thread.start()
+    
+    def _command(self, msg, flag="-m"):
         if flag == "-m":
             r = self._send_and_read(msg)
         elif flag == "-s":
@@ -36,23 +40,28 @@ class EspConnection:
         else:
             r = self._send_and_read(msg)
         return r
-
+    
     def get_configs(self, config_name: str = "main"):
+        thread = threading.Thread(target=self.get_configs, args=(config_name,))
+        thread.start()
+    
+    
+    def _get_configs(self, config_name: str = "main"):
         if not config_name in ["temp", "led", "main"]:
             config_name = "main"
         r = self._send_and_read(f"settings get_{config_name}_json")
         d = json.loads(r)
         if type(d) == dict:
             self[config_name] = d
-
+    
     def __getitem__(self, item):
         if item == "ip":
             return self.ip
         return self.data[item]
-
+    
     def __setitem__(self, key, value):
         if key == "ip":
             self.ip = value
         else:
             self.data[key] = value
-
+    
