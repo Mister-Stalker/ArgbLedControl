14630e7bdf38a6d8ccd6d2e2a00595f6d70b38f3
Mister-Stalker <pna24052006@yandex.ru>
2022-03-30 15:07:39 +0300

    v0.6.1
    Поддержка нескольких плат

--- esp_connection.py
+++ esp_connection.py
@@ -5,7 +5,7 @@ import threading
 
 
 class EspConnection:
-    def __init__(self, app_ids, ip="192.168.0.123", port=80):
+    def __init__(self, app_ids, ip="192.168.0.201", port=80):
         self.ip = ip
         self.app_ids = app_ids
         self.port = port
@@ -13,11 +13,18 @@ class EspConnection:
             "temp": {},
             "main": {},
             "led": {},
-            "lock": False,  # блокировка при обмене данных для избежания сбоев
+            "lock": True,  # блокировка при обмене данных для избежания сбоев
             "brightness": 10,
         }
     
     def _send_and_read(self, msg: str, sleep=0.5, read=True):
+        if self["lock"]:
+            print(f"ESP ip: {self.ip}   config: {self.data}")
+            self.app_ids.lock_label.text = "LOCK"
+            time.sleep(2)
+            self.app_ids.lock_label.text = ""
+            return "{}"
+        self["lock"] = True
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((self.ip, self.port))
         msg = msg.encode()
@@ -26,9 +33,11 @@ class EspConnection:
             time.sleep(sleep)
             r = sock.recv(4096).decode()
             return r
+        self["lock"] = False
         
     def command(self, msg, flag="-m"):
-        thread = threading.Thread(target=self._command, args=(command, flag))
+        thread = threading.Thread(target=self._command, args=(msg, flag))
+        print(f"send msg to esp {msg} {flag}")
         thread.start()
     
     def _command(self, msg, flag="-m"):
@@ -42,8 +51,9 @@ class EspConnection:
             r = self._send_and_read(msg)
         return r
     
-    def get_configs(self, config_name: str = "main"):
-        thread = threading.Thread(target=self.get_configs, args=(config_name,))
+    def load_config(self, config_name: str = "main"):
+        thread = threading.Thread(target=self._get_config, args=(config_name,))
+
         thread.start()
     
     def set_brig(self, *arg):
@@ -57,7 +67,7 @@ class EspConnection:
         else:
             self.app_ids.lock_label.text = ""
         
-    def _get_configs(self, config_name: str = "main"):
+    def _get_config(self, config_name: str = "main"):
         if not config_name in ["temp", "led", "main"]:
             config_name = "main"
         r = self._send_and_read(f"settings get_{config_name}_json")
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import json
 import socket
 import time
+from kivy.properties import StringProperty
+
+from kivymd.uix.list import OneLineIconListItem
 from kivy.app import async_runTouchApp
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty
@@ -13,13 +16,16 @@ from kivymd.uix.button import MDIconButton
 from kivymd.uix.menu import MDDropdownMenu
 import threading
 from esp_connection import EspConnection
-
+from kivy.metrics import dp
 Config.set('kivy', 'window_icon', 'icon.png')
 
 
 class ArgbLedControl(MDApp):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
     def build(self):
+
         self.theme_cls.material_style = "M3"
         self.theme_cls.theme_style = "Dark"
         # self.theme_cls.primary_palette = "DeepOrange"
@@ -28,13 +34,36 @@ class ArgbLedControl(MDApp):
         # return Builder.load_file("app_interface.kv")
         self.root = Builder.load_file("app_interface.kv")
         self.root.ids.debug_label.text = "DEBUG"
-        # self.esp = EspConnection(self.root.ids)
+
+        menu_items = [
+            {
+                "viewclass": "IconListItem",
+                "icon": "git",
+                "text": ip,
+                "height": dp(56),
+                "on_release": lambda x=ip: self.set_ip(x),
+            } for ip in ["192.168.0.201", "192.168.0.202"]
+        ]
+
+        self.menu = MDDropdownMenu(
+            caller=self.root.ids.drop_item,
+            items=menu_items,
+            position="center",
+            width_mult=4,
+        )
+        self.menu.bind()
+
+    def set_ip(self, text_item):
+        self.root.ids.drop_item.set_item(text_item)
+        self.menu.dismiss()
+        self.root.esp["ip"] = text_item
+    # self.esp = EspConnection(self.root.ids)
 
 
 class MDScreenMain(MDScreen):
     def __init__(self, *args, **kwargs):
         super(MDScreenMain, self).__init__(*args, **kwargs)
-        self.esp = EspConnection(self.root.ids)
+        self.esp = EspConnection(self.ids)
 
     def set_ip(self, text_item):  # for MDDropdownMenu
         self.esp.ip = text_item
@@ -56,6 +85,8 @@ class ContentNavigationDrawer(MDBoxLayout):
     screen_manager = ObjectProperty()
     nav_drawer = ObjectProperty()
 
+class IconListItem(OneLineIconListItem):
+    icon = StringProperty()
 
 class ModeButton(Button):
     pass
